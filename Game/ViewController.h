c0436463c5fb8bf34ab3d64f8f2c55de4ebcb180
Lee Jian Yi David <ljydavid@soctf-imac-023.comp.nus.edu.sg>
2013-01-28 14:05:44 +0800

    Initial Commit

---
+++ Game/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Game
+//
+//  Created by Lee Jian Yi David on 1/28/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Game/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Game
+//
+//  Created by Lee Jian Yi David on 1/28/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
